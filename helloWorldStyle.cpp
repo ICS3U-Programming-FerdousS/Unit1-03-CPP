ae1a63ac6a840fa671e29675daf4fd90fc4f1d1e
Ubuntu <ubuntu@ip-172-31-46-166.ec2.internal>
2022-02-20 00:00:57 +0000

    Added style cheacker for cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Ms Raffin All rights reserved.
+// .
+// Created by: Ferdous Sediqi
+// Date: Feb. 18, 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with a proper style!" << std::endl;
+}
